f3cb30a453fe3b6d79c4d93fac760267205c3922
Matt Pascoe <matt@opennetadmin.com>
2023-09-26 20:00:01 -0600

    initial nix state.  much more to do, nothing really works just yet

---
+++ .config/home-manager/flake.nix
@@ -0,0 +1,37 @@
+# This file strictly manages standalone home stuff for now
+{
+  description = "Home Manager configuration of mdp";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    #nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
+
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+# For now, comment out darwin as I think this needs to live in .config/darwin separately
+#    darwin = {
+#      url = "github:lnl7/nix-darwin";
+#      inputs.nixpkgs.follows = "nixpkgs";
+#    };
+ };
+
+  outputs = { nixpkgs, home-manager, ... }:
+    let
+      system = "x86_64-linux";
+      pkgs = nixpkgs.legacyPackages.${system};
+    in {
+      homeConfigurations."mdp" = home-manager.lib.homeManagerConfiguration {
+        inherit pkgs;
+
+        # Specify your home configuration modules here, for example,
+        # the path to your home.nix.
+        modules = [ ./home.nix ];
+
+        # Optionally use extraSpecialArgs
+        # to pass through arguments to home.nix
+      };
+    };
+}
