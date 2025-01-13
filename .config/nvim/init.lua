aec0890fac8c00107173ee003c508d781013ef57
Matt Pascoe <matt@opennetadmin.com>
2025-01-12 19:20:06 -0700

    Adding vim-mergetool

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -979,6 +979,18 @@ require('lazy').setup({
   },
 
   {
+    'samoshkin/vim-mergetool',
+    init = function()
+      vim.g.mergetool_layout = 'mr'
+      vim.g.mergetool_prefer = 'local'
+      vim.keymap.set('n', '<leader>mt', ':MergetoolToggle<cr>', { desc = 'Merge Tool' })
+      vim.keymap.set('n', '<leader>ml', ':MergetoolExchangeLeft<cr>', { desc = 'Merge to Left' })
+      vim.keymap.set('n', '<leader>mr', ':MergetoolExchangeRight<cr>', { desc = 'Merge to Right' })
+      vim.keymap.set('n', '<leader>mc', ':MergetoolStop<cr>', { desc = 'Merge Complete' })
+    end,
+  },
+
+  {
     'vimwiki/vimwiki',
     init = function()
       vim.g.vimwiki_list = {
