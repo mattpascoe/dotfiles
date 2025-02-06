71fcb840cf28128a5bed47f85aa666c55b7cf933
Matt Pascoe <matt@opennetadmin.com>
2025-02-05 20:35:56 -0700

    Adding after filetype plugins. php formatting to start

---
+++ .config/nvim/after/ftplugin/php.lua
@@ -0,0 +1,9 @@
+local set = vim.opt_local
+
+-- Attempting to define some standard PHP settings
+-- Just know that nvim-sluth is probably going to make its own updates
+set.shiftwidth = 4
+set.tabstop = 4
+set.smartindent = false
+set.autoindent = true
+-- set.cinoptions = vim.opt.cinoptions:get() .. ',+s'
