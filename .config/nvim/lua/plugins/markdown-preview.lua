e113bacf05b25a0066fdebe63cd7a3cf1da8da8b
Matt Pascoe <matt@opennetadmin.com>
2025-05-20 21:14:39 -0600

    nvim: markdown-preview comment

--- .config/nvim/lua/plugins/markdown-preview.lua
+++ .config/nvim/lua/plugins/markdown-preview.lua
@@ -1,6 +1,9 @@
 return {
   -- Preview markdown live in web browser
   -- NOTE: had to do a ':Lazy build markdown-preview.nvim' the first time
+  -- Apparently this is no longer maintained. Some other things to look at are:
+  -- live-preview https://github.com/brianhuster/live-preview.nvim
+  --    this one is nice as it uses lua instead of nodejs crap
   {
     'iamcco/markdown-preview.nvim',
     cond = function()
