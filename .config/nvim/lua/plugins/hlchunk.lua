90211b62ca2ce3b415abdc6eba4c892ae0448688
Matt Pascoe <matt@opennetadmin.com>
2025-06-26 17:25:00 -0600

    nvim: test if nvim version is 10 or more for hlchunk

--- .config/nvim/lua/plugins/hlchunk.lua
+++ .config/nvim/lua/plugins/hlchunk.lua
@@ -1,35 +1,40 @@
-return {
-  {
-    -- Highlight chunk of code, this will show indentations as well
-    'shellRaining/hlchunk.nvim',
-    event = { 'BufReadPre', 'BufNewFile' },
-    config = function()
-      require('hlchunk').setup({
-        chunk = {
-          enable = false,
-          delay = 0,
-        },
-        indent = {
-          enable = true,
-          style = '#1c1f21',
-        },
-        line_num = {
-          enable = true,
-          style = '#806d9c',
-        },
-        blank = {
-          enable = false,
-          chars = {
-            ' ',
+-- Only load this if we have nvim 10 or newer
+if vim.fn.has('nvim-0.10') == 1 then
+  return {
+    {
+      -- Highlight chunk of code, this will show indentations as well
+      'shellRaining/hlchunk.nvim',
+      event = { 'BufReadPre', 'BufNewFile' },
+      config = function()
+        require('hlchunk').setup({
+          chunk = {
+            enable = false,
+            delay = 0,
           },
-          style = {
-            { bg = '#434437' },
-            { bg = '#2f4440' },
-            { bg = '#433054' },
-            { bg = '#284251' },
+          indent = {
+            enable = true,
+            style = '#1c1f21',
           },
-        },
-      })
-    end,
-  },
-}
+          line_num = {
+            enable = true,
+            style = '#806d9c',
+          },
+          blank = {
+            enable = false,
+            chars = {
+              ' ',
+            },
+            style = {
+              { bg = '#434437' },
+              { bg = '#2f4440' },
+              { bg = '#433054' },
+              { bg = '#284251' },
+            },
+          },
+        })
+      end,
+    },
+  }
+else
+  return {}
+end
