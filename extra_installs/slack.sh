bb1100279707a5c4fa0c08dde90abfbbcb8717da
Matt Pascoe <matt@opennetadmin.com>
2025-10-18 22:26:48 -0600

    slack install deps

--- extra_installs/slack.sh
+++ extra_installs/slack.sh
@@ -9,6 +9,8 @@ case "$ID" in
     if [[ $ARCH == "arm64" ]]; then
       echo "-!- Slack debian not supported on arm64."
     else
+      # Ensure dependencies are installed
+      sudo apt install libxss1
       # Lame extract of latest version of the deb file. Probably will break in the future
       # The .deb file looks to be amd64 type only.
       curl -s "https://slack.com/downloads/instructions/linux?ddl=1&build=deb" \
