671b9885a82ddcfd0a0c7735a1f025ba654159a2
Matt Pascoe <matt@opennetadmin.com>
2025-10-19 17:08:29 -0600

    kanata: no really

--- extra_installs/kanata.sh
+++ extra_installs/kanata.sh
@@ -12,13 +12,14 @@ case "$ID" in
     tmpdir=$(mktemp -d)
     ARCH=${ARCH:-$(uname -m)}; ARCH=${ARCH/aarch64/arm64}
     if [ "$ARCH" != "arm64" ]; then
+      echo 'Installing kanata...'
       VERSION=$(curl -s https://api.github.com/repos/jtroo/kanata/releases/latest | grep -Po '"tag_name": "v\K[0-9.]+')
       wget -q -P "$tmpdir" https://github.com/jtroo/kanata/releases/download/v"${VERSION}"/kanata
       sudo install -b "$tmpdir"/kanata /usr/local/bin/kanata
       rm -rf "$tmpdir"
 
       sudo rm -f /etc/kanata.cfg
-      sudo ln -s ~/.config/kanata.cfg /etc/kanata.cfg
+      sudo ln -s ~/.config/kanata/kanata.kbd /etc/kanata.cfg
       sudo tee /etc/systemd/system/kanata.service > /dev/null <<'EOF'
 [Unit]
 Description=Kanata keyboard remapping daemon
