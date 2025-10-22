30930f6d03bab73fc75a70a814b267ad97982a07
Matt Pascoe <matt@opennetadmin.com>
2025-10-22 12:02:31 -0600

    gnome: several updates trying to fix keybinds and ghostty

--- .gnome.sh
+++ .gnome.sh
@@ -46,31 +46,26 @@ if command -v "gsettings" &> /dev/null; then
   gsettings set org.gnome.desktop.wm.keybindings close "['<Super>q']"
 
   # Experiment with some remapping of default keys to my own taste.  Makes it more Mac like?? is that a bad idea?
-  gsettings set org.gnome.settings-daemon.plugins.media-keys screensaver "[]" # remove default binding
-  gsettings set org.gnome.shell.keybindings focus-active-notification "[]" # remove default binding
+  gsettings set org.gnome.settings-daemon.plugins.media-keys screensaver "[]" # remove default binding to free for other things
+  gsettings set org.gnome.shell.keybindings focus-active-notification "[]" # remove default binding to free for other things
   gsettings set org.gnome.shell.keybindings toggle-message-tray "['<Super>n']" # change to n for notifications
   gsettings set org.gnome.desktop.wm.keybindings maximize "['<Super>k']"
   gsettings set org.gnome.desktop.wm.keybindings minimize "['<Super>j']"
   gsettings set org.gnome.shell.extensions.tiling-assistant tile-left-half "['<Super>Left', '<Super>KP_4', '<Super>h']"
   gsettings set org.gnome.shell.extensions.tiling-assistant tile-right-half "['<Super>Right', '<Super>KP_6', '<Super>l']"
 
-
-
   # Careful here.. need to test this more
   # There is other info about adjusting all GTK apps using this
   # https://unix.stackexchange.com/questions/342628/gnome-3-keybindings-in-source-where-are-ctrl-c-cut-copy-and-paste-defin/399632
-  #gsettings set org.gnome.Ptyxis.Shortcuts paste-clipboard '<Super>v'
+  if gnome-extensions list --enabled | grep -q "org.gnome.Ptyxis"; then
+    gsettings set org.gnome.Ptyxis.Shortcuts paste-clipboard '<Super>v'
+    gsettings set org.gnome.Ptyxis.Shortcuts copy-clipboard '<Super>c'
+  fi
   #gsettings set org.gnome.Terminal.Legacy.Keybindings paste '<Super>v'
-  #gsettings set org.gnome.Ptyxis.Shortcuts copy-clipboard '<Super>c'
   #gsettings set org.gnome.Terminal.Legacy.Keybindings copy '<Super>c'
   # This seems to work well for terminal.
   dconf write /org/gnome/terminal/legacy/keybindings/copy  \'"<Super>c"\'
   dconf write /org/gnome/terminal/legacy/keybindings/paste \'"<Super>v"\'
-  #default values
-  #org.gnome.Ptyxis.Shortcuts paste-clipboard '<ctrl><shift>v'
-  #org.gnome.Terminal.Legacy.Keybindings paste '<Control><Shift>v'
-
-
 
   # Dock settings
   if gnome-extensions list --enabled | grep -q "ubuntu-dock@ubuntu.com"; then
@@ -81,35 +76,41 @@ if command -v "gsettings" &> /dev/null; then
     gsettings set org.gnome.shell.extensions.dash-to-dock extend-height false # panel mode
   fi
 
+  # Make sure scrolling is natural
   gsettings set org.gnome.desktop.peripherals.mouse natural-scroll true
   gsettings set org.gnome.desktop.peripherals.touchpad natural-scroll true
 
   # Center new windows in the middle of the screen
   gsettings set org.gnome.mutter center-new-windows true
-  # Set Cascadia Mono as the default monospace font
-  #gsettings set org.gnome.desktop.interface monospace-font-name 'MesloLGM Nerd Font 10'
   # Reveal week numbers in the Gnome calendar
   gsettings set org.gnome.desktop.calendar show-weekdate true
   # Turn off ambient sensors for setting screen brightness (they rarely work well!)
   gsettings set org.gnome.settings-daemon.plugins.power ambient-enabled false
 
+  # Disable bell
+  gsettings set org.gnome.wm.preferences audible-bell false
+
   # Adjust switching of windows in an app group to MEH-Tab
   gsettings set org.gnome.desktop.wm.keybindings switch-panels-backward "[]"
   gsettings set org.gnome.desktop.wm.keybindings switch-group "['<Super>Above_Tab', '<Alt>Above_Tab', '<Shift><Control><Alt>Tab']"
 
+
   # Reserve slots for custom keybindings
   gsettings set org.gnome.settings-daemon.plugins.media-keys custom-keybindings "['/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/', '/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom1/']"
 
+  # Instead of ROFI lets just keep it simple and use the built in app search mess.  Its harsh but much simpler to deal with for as much as I actually use it.
+  gsettings set org.gnome.shell.keybindings toggle-overview "['<Super>space']"
   # Set ROFI custom keybind
   # First remove existing keybind
-  gsettings set org.gnome.desktop.wm.keybindings switch-input-source "@as []"
-  # Set our new keybind
-  gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/ name "App Launcher"
-  gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/ command "rofi -show drun -normal-window"
-  gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/ binding "<Super>space"
-  #dconf write /org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/name "'Rofi Launcher'"
-  #dconf write /org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/command "'rofi -show drun'"
-  #dconf write /org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/binding "'<Super>space'"
+  #gsettings set org.gnome.desktop.wm.keybindings switch-input-source "@as []"
+  ## Set our new keybind
+  #gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/ name "App Launcher"
+  #gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/ command "rofi -show drun -normal-window"
+  #gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/ binding "<Super>space"
+  ##dconf write /org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/name "'Rofi Launcher'"
+  ##dconf write /org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/command "'rofi -show drun'"
+  ##dconf write /org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/binding "'<Super>space'"
+
   # Keybind for ghostty that for now will set LIBGL_ALWAYS_SOFTWARE=1 because of horrible rendering issues on all my boxes. This is NON OPTIMAL
   gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom1/ name "Ghostty terminal"
   gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom1/ command "bash -c 'LIBGL_ALWAYS_SOFTWARE=1 ghostty'"
@@ -122,7 +123,6 @@ if command -v "gsettings" &> /dev/null; then
   # set default terminal?
   # Having issues and having to start it with LIBGL_ALLOW_SOFTWARE=1 ghostty
   #gsettings set org.gnome.desktop.default-applications.terminal exec "ghostty"
-  gsettings set org.gnome.Terminal.Legacy.Profile:/org/gnome/terminal/legacy/profiles:/:b1dcc9dd-5262-4d8d-a863-c897e6d979b9/ background-color '#000000'
 
   # set background
   gsettings set org.gnome.desktop.background picture-uri "file://${HOME}/.wallpaper.png"
@@ -133,10 +133,12 @@ if command -v "gsettings" &> /dev/null; then
   gsettings set org.gnome.desktop.interface enable-animations false
 
   # Setup hotkeys for applications. Requires application-hotkeys extension to be installed
+  # I'm using this instead of custom-keybinds because it knows to open vs focus.
   # TODO: could these all just be custom-keybindings instaead?
   dconf write /org/gnome/shell/extensions/application-hotkeys/configs "@as [
     '[\"org.gnome.Terminal.desktop\",    \"<Shift><Control><Alt>t\"]',
     '[\"brave-browser.desktop\",         \"<Shift><Control><Alt>b\"]',
+    '[\"com.mitchellh.ghostty.desktop\", \"<Shift><Control><Alt>g\"]',
     '[\"1password.desktop\",             \"<Shift><Control><Alt>p\"]'
   ]"
 fi
