2548a3ebf9e6c9e5e1bb9c3a507fd8c41e20b670
Matt Pascoe <matt@opennetadmin.com>
2025-12-18 15:39:36 -0700

    lazygit: install in home/bin dir instead of system wide

--- setup/profiles/lazygit.sh
+++ setup/profiles/lazygit.sh
@@ -12,10 +12,9 @@ case "$ID" in
     VERSION=$(curl -s https://api.github.com/repos/jesseduffield/lazygit/releases/latest | grep -Po '"tag_name": "v\K[0-9.]+')
     wget -q -P "$tmpdir" https://github.com/jesseduffield/lazygit/releases/latest/download/lazygit_"${VERSION}"_linux_"${ARCH}".tar.gz
     tar xf "$tmpdir/lazygit"*.tar.gz -C "$tmpdir" lazygit
-    # Move to a system-wide location
-    sudo install -b "$tmpdir"/lazygit /usr/local/bin
-    rm -rf "$tmpdir"
+    install -b "$tmpdir/${PKG_NAME}" "$HOME/bin"
     link_file ".config/$PKG_NAME"
+    rm -rf "$tmpdir"
     ;;
   macos*)
     brew install "$PKG_NAME" 2>&1|sed '/^To reinstall/,$d'
