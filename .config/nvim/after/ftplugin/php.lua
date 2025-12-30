3e8038c284619d3fc70c9bc8ea937be7e3fb1d70
Matt Pascoe <matt@opennetadmin.com>
2025-12-30 10:42:19 -0700

    nvim: php settings to keep space indent like PHPStorm

--- .config/nvim/after/ftplugin/php.lua
+++ .config/nvim/after/ftplugin/php.lua
@@ -7,3 +7,15 @@ set.tabstop = 4
 set.smartindent = false
 set.autoindent = true
 -- set.cinoptions = vim.opt.cinoptions:get() .. ',+s'
+
+-- For PHP files only, lets ensure blank lines are indented with spaces
+-- This is how Brandon likes the format so its like PHPStorm.
+-- Set buffer-local mappings (buffer = 0 or true refers to the current file)
+local opts = { noremap = true, silent = true, buffer = true }
+
+-- Insert mode: Enter key keeps the indent
+vim.keymap.set('i', '<CR>', '<C-o>o=<BS>', opts)
+
+-- Normal mode: 'o' and 'O' keep the indent
+vim.keymap.set('n', 'o', 'ox<BS>', opts)
+vim.keymap.set('n', 'O', 'Ox<BS>', opts)
