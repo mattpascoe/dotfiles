04aa92bf955418623cb9be8d5e261fd67fe3a8d4
Matt Pascoe <matt@opennetadmin.com>
2026-02-03 09:44:19 -0700

    role: adding new role for church laptop

---
+++ setup/roles/church.sh
@@ -0,0 +1,40 @@
+#!/usr/bin/env bash
+# Linux laptop for presentation slides at church
+
+PROFILES=(
+  starship
+  fzf
+  tmux
+  nerdfonts
+  lazygit
+  neovim
+  desktop
+  kanata
+  brave-browser
+  ghostty
+)
+
+run_profiles "${PROFILES[@]}"
+
+# Setup some common config symlinks
+msg "Checking dotfile config symlinks"
+link_file ".config/btop"
+link_file ".config/git"
+link_file ".profile"
+link_file ".vimrc"
+link_file ".zshrc"
+
+# Define a few other specifics just for this setup this is all hard coded!
+
+# disable notifications, AKA do not disturb
+gsettings set org.gnome.desktop.notifications show-banners false
+# set screen sleeps and stuff
+gsettings set org.gnome.settings-daemon.plugins.power sleep-inactive-ac-timeout 7200
+# Dont suspend when on AC power
+gsettings set org.gnome.settings-daemon.plugins.power sleep-inactive-ac-type 'nothing'
+
+# Disable screen blank
+gsettings set org.gnome.desktop.session idle-delay 0
+
+# Install some apps
+sudo apt install vlc libreoffice
