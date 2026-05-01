2508d14871eb1a36cf7ff0b45ab4813e2558bc80
Matt Pascoe <matt@opennetadmin.com>
2026-04-30 21:30:11 -0600

    hammerspoon: global level hotkeys to jump to tmux sessions

--- .hammerspoon/init.lua
+++ .hammerspoon/init.lua
@@ -8,6 +8,44 @@ MEH = { "shift", "ctrl", "alt" }
 -- hs.ipc.cliInstall("/opt/homebrew/bin")
 -- require("hs.ipc")
 
+-- Timers could get messy here but so far its working.  slight lag but usable.  could tune down further maybe?
+-- may interfear with my nvim space+w save option if I dont pause on my space hold too long
+local function gotoTmuxGroup(key)
+	local app = hs.application.get("Ghostty")
+
+	if not app then
+		hs.application.launchOrFocus("Ghostty")
+		hs.timer.usleep(5000)
+		app = hs.application.get("Ghostty")
+	end
+
+	app:activate(true)
+	hs.timer.usleep(5000)
+
+	hs.eventtap.keyStroke({ "ctrl" }, "space")
+	hs.timer.usleep(1000)
+
+	hs.eventtap.keyStroke({}, "g")
+	hs.timer.usleep(1000)
+
+	hs.eventtap.keyStroke({}, key)
+end
+
+hs.hotkey.bind(MEH, "w", function()
+	gotoTmuxGroup("w")
+end)
+hs.hotkey.bind(MEH, "h", function()
+	gotoTmuxGroup("h")
+end)
+hs.hotkey.bind(MEH, "r", function()
+	gotoTmuxGroup("r")
+end)
+
+-- hs.hotkey.bind(MEH, "w", function()
+-- 	-- hs.execute("/Applications/Ghostty.app/Contents/MacOS/ghostty -e tmux new-session -A -s Wiki > /dev/null 2>&1 &")
+-- 	hs.execute("open -a Ghostty --args -e tmux new-session -A -s Wiki")
+-- end)
+
 -- Define app hotkeys in a table
 -- This will add a binding for MEH + key
 local appHotkeys = {
