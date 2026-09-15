5f66756af655dfa332a4dd04c9ef2b32240f0c39
Matt Pascoe <matt@opennetadmin.com>
2026-09-14 20:02:41 -0600

    setup: adding aven profile and configs

---
+++ setup/profiles/aven.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+# Local task manager TUI
+
+PKG_NAME=aven
+case "$ID" in
+  arch*)
+    # shellcheck disable=SC2086
+    $PLATFORM_INSTALLER_BIN $INSTALLER_OPTS "$PKG_NAME" ;;
+  debian*|ubuntu*)
+    tmpdir=$(mktemp -d)
+    ARCH=${ARCH:-$(uname -m)}; ARCH=${ARCH/aarch64/arm64}
+    VERSION=$(curl -s https://api.github.com/repos/raine/aven/releases/latest | grep -Po '"tag_name": "v\K[0-9.]+')
+    # Download and extract
+    wget -q -P "$tmpdir" https://github.com/raine/aven/releases/latest/download/aven-v"${VERSION}"-linux-"${ARCH}".tar.gz
+    tar xf "$tmpdir/aven"*.tar.gz -C "$tmpdir" ${PKG_NAME}
+    install -b "$tmpdir/${PKG_NAME}" "$HOME/bin"
+    link_file ".config/$PKG_NAME"
+    rm -rf "$tmpdir"
+    ;;
+  macos*)
+    # shellcheck disable=SC2086
+    $PLATFORM_INSTALLER_BIN install $INSTALLER_OPTS "$PKG_NAME" 2>&1|sed '/^To reinstall/,$d'
+    link_file ".config/$PKG_NAME"
+    ;;
+  *)
+    echo "-!- Install not supported."
+    ;;
+esac
+
+msg "${BLU}Install complete."
